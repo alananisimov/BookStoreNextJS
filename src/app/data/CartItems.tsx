ba97ec8522dd45078f1a2d8f73faeccdfa9c6b0c
alananisimov <alananisimov@gmail.com>
2023-08-09 19:01:34 +0300

    ertt

--- src/app/data/CartItems.tsx
+++ src/app/data/CartItems.tsx
@@ -10,19 +10,17 @@ export default function CartItems(){
    
     return(
         <>
-        {cart_items?.length != 0 ? cart_items?.map((product, index) => <div className="flex flex-col rounded-lg bg-white sm:flex-row" key={index}>
-          <div className="inline-flex md:flex">
-        <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={product.image} alt="" />
-        <button className="btn w-20 ml-auto md:hidden float-left"><RemoveItem name={product.title} symbol="Удалить"/></button>
-        </div>
-        <div className="flex w-full flex-col px-4 py-4">
-          <div className="inline-flex">
+        {cart_items?.length != 0 ? cart_items?.map((product, index) => <div className="flex flex-col rounded-lg bg-white flex-row" key={index}>
+          <div className="inline-flex gap-5">
+          <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src={product.image} alt="" />
+          <div className="flex-col">
           <span className="font-semibold">{product.title}</span>
-          <button className="btn w-20 ml-6 hidden md:inline-flex"><RemoveItem name={product.title} symbol="Удалить"/></button>
-          </div>
-          <p className="text-lg font-bold">${product.price}</p>
           
+          <p className="text-lg font-bold">${product.price}</p>
+          </div>
+        <button className="btn w-20 ml-auto float-left"><RemoveItem name={product.title} symbol="Удалить"/></button>
         </div>
+        
       </div>): 
       <div className="flex flex-col rounded-lg bg-white sm:flex-row">
        <img className="m-2 h-24 w-28 rounded-md border object-cover object-center" src="https://i.ibb.co/mCTDNvJ/empty-4731755.png"/>
