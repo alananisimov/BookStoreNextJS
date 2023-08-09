b25f4bc2e5bc2fb741a7f9b149f9563133164ca9
alananisimov <alananisimov@gmail.com>
2023-08-09 18:43:58 +0300

    sdff

--- src/components/QuickView.tsx
+++ src/components/QuickView.tsx
@@ -29,7 +29,6 @@ function classNames(...classes: string[]) {
 export default function QuickView({product_props, open, setOpen, usePlus, setPlus}: {product_props: MyProduct, open: boolean, setOpen: Dispatch<SetStateAction<boolean>>, usePlus: number, setPlus: Dispatch<SetStateAction<number>>}) {
   
   const [Myitems, setItems] = useLocalStorage<Array<String>>('items', [])
-  const cart_items = useReadLocalStorage<Object[]>('items')
 
   //написать статью
   //написать статью
@@ -40,15 +39,6 @@ export default function QuickView({product_props, open, setOpen, usePlus, setPlu
   setItems(prevent =>[...prevent, de])
   }
 
-const product = {
-  name: product_props.title,
-  price: product_props.price,
-  rating: product_props.rating,
-  reviewCount: 117,
-  href: '#',
-  imageSrc: product_props.image,
-  imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
-}
   
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -90,17 +80,17 @@ const product = {
 
                   <div className="grid w-full grid-cols-1 items-center gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
                     <div className="aspect-h-3 aspect-w-2 overflow-hidden col-span-2 rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
-                      <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center"/>
+                      <img src={product_props.image} alt={product_props.image} className="object-cover object-center"/>
                     </div>
                     <div className="sm:col-span-8 lg:col-span-7">
-                      <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
+                      <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product_props.title}</h2>
 
                       <section aria-labelledby="information-heading" className="mt-2">
                         <h3 id="information-heading" className="sr-only">
                           Product information
                         </h3>
 
-                        <p className="text-2xl text-gray-900">{product.price} USD</p>
+                        <p className="text-2xl text-gray-900">{product_props.price} USD</p>
 
                         {/* Reviews */}
                         <div className="mt-6">
@@ -111,16 +101,16 @@ const product = {
                                 <StarIcon
                                   key={rating}
                                   className={classNames(
-                                    product.rating.rate > rating ? 'text-gray-900' : 'text-gray-200',
+                                    product_props.rating.rate > rating ? 'text-gray-900' : 'text-gray-200',
                                     'h-5 w-5 flex-shrink-0'
                                   )}
                                   aria-hidden="true"
                                 />
                               ))}
                             </div>
-                            <p className="sr-only">{product.rating.count} out of 5 stars</p>
+                            <p className="sr-only">{product_props.rating.count} out of 5 stars</p>
                             <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                              {product.reviewCount} reviews
+                              117 reviews
                             </a>
                           </div>
                         </div>
@@ -139,7 +129,7 @@ const product = {
                             
                           </div>
                           {usePlus == 0 ? <button
-                            onClick={()=>{addItem(product)
+                            onClick={()=>{addItem(product_props)
                               setPlus(prevent => prevent +1 )
                             }}
                             className="bottom-0 mt-20 mr-10 flex w-[100%] items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -151,14 +141,14 @@ const product = {
                             <button className='w-10 h-10 hover:bg-indigo-800 rounded-md'
                             onClick={()=>{
                               setPlus(prevent => prevent+1)
-                              addItem(product)
+                              addItem(product_props)
                               }}>
                                 +
                               </button>
                               <div className='mx-auto justify-center text-center items-center'>
                               {usePlus}
                               </div>
-                            <button className='w-10 h-10 hover:bg-indigo-800 rounded-md' onClick={()=>{setPlus(prevent => prevent-1)}}><RemoveItem name={product.name} symbol='-'/></button>
+                            <button className='w-10 h-10 hover:bg-indigo-800 rounded-md' onClick={()=>{setPlus(prevent => prevent-1)}}><RemoveItem name={product_props.title} symbol='-'/></button>
                           </div>
                           
                           </div>}
